72e5a7b3e0cf1d6ca9ed7f9a28ac27b43d39016c
Matheus Satelos <matheus.satelos@gmail.com>
2020-04-26 21:23:09 -0300

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="container"></div>
+  </footer>
+</template>
+
+<script lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/sass/_variables.scss";
+footer {
+  background-color: $white;
+  display: flex;
+  align-items: center;
+  padding: 30px 0;
+  margin: 15px 0 0;
+  padding: 30px 20px 45px;
+  height: 300px;
+}
+</style>
